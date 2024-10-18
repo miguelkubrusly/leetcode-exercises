09d244ace016b38e75bc757dd6bea83988a77b21
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-18 19:37:16 -0300

    fix(arr/str): touche. explanation is commented

--- arrays_and_strings/274_H-Index_medium.py
+++ arrays_and_strings/274_H-Index_medium.py
@@ -1,7 +1,7 @@
 # 274. H-Index
 # Medium
 
-# Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+# Given an array of integers citations where citations[index] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
 
 # According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
 
@@ -20,38 +20,42 @@
 
 # n == citations.length
 # 1 <= n <= 5000
-# 0 <= citations[i] <= 1000
+# 0 <= citations[index] <= 1000
 
 
 
 #PERSONAL NOTES
 
 ## number of papers = array.length
-## array[i] = citations for this article
+## array[index] = citations for this article
 ## H-index = max(h)
 ## h = num of papers with >= h citations!
 #idea:
-##1- check numbers, from highest to lowest value, until we find h
-###1.1-sort in descendent order
-###1.2- check how many papers they have higher than current citation value in iteration
-###1.3-if num of papers with citations higher than current == num of citations: there is you h 
-##2- since h was found in desc order, h is the max h
-
+## sort desc.
+## quantity of valid articles per quantity of citations is equal to index+1 
+## the last value in which qty of citations is equal or greater to qty of valid articles will be the max H-index.
+## the way I best understood this is thinking that a person with 1000000 articles, with 1 citation each, would have the H-index of one. and the inverse is also true
+## so, when sorted desc., the farthest in the list we can secure a qty of citations higher or equal to index+1 (qty of articles) will necessarily be the highest number we can get
+## since this will be the point where the other value limits his brother the least (highest acceptable number). we start with the least value of articles and add to it until we can  
 ######################################################################################
+##wrong idea in previous commit, 37c2dbdca71d481c84c245d7f93a80fff1f2d0f5
 
 
-def hIndex( citations: list[int]) -> int:
+def hIndex(citations):
   citations.sort(reverse=True)
-  # possible_hs = []
-  max_h = 0
-  for i, this_citation in enumerate(citations):
-    h = 0
-    for next_citation in citations[i+1:]:
-      if next_citation >= this_citation:
-        h+=1
-      if this_citation>=h:
-        max_h=max(max_h,this_citation)
-      print(f"h: {h}; max_h: {max_h}; citations[i+1:]: {citations[i+1:]}; this_citation: {this_citation}; next_citation: {next_citation}; i: {i}")
-  return max_h
+  n = len(citations)
 
+  h_index = 0
+  for index, c in enumerate(citations):
+    if c >= index + 1:
+      h_index+=1
+    else: 
+      break
+
+  return h_index
+
+print("Should be 3:")
 print(hIndex([3,0,6,1,5]))
+print("Should be 1:")
+print(hIndex([1,3,1]))
+  
